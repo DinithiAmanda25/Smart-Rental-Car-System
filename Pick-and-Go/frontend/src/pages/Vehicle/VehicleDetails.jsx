4fd0033559d56845954e76e3d439347872c16471
Ayesha <dilandiayesha@gmail.com>
2025-08-13 13:58:47 +0530

    create vehicle details page

---
+++ Pick-and-Go/frontend/src/pages/Vehicle/VehicleDetails.jsx
@@ -0,0 +1,3 @@
+<div>
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro recusandae ipsa distinctio minus hic, at natus. Quod eius quaerat officiis velit quae repellat molestias optio corporis sunt aliquid, delectus commodi!
+</div>
