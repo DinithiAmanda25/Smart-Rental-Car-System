8f7d4a5d4d86e8257f6f41581d31817a9d9e638f
Dineesha-1 <ishtamithra@gmail.com>
2025-08-13 11:54:49 +0530

    booking page

---
+++ Pick-and-Go/frontend/src/pages/Booking/Booking.jsx
@@ -0,0 +1,3 @@
+<div>
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas recusandae officiis tenetur facilis veritatis. Ut dicta, consequuntur assumenda fuga reprehenderit sit hic ducimus unde qui eveniet, enim quibusdam totam minus.
+</div>
