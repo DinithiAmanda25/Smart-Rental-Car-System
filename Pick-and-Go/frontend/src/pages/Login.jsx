ea506a89d0bc19674ede5e727fb41c8be2737014
fuji289 <nethvidusl@gmail.com>
2025-08-13 11:05:11 +0530

    i create login.jsx

---
+++ Pick-and-Go/frontend/src/pages/Login.jsx
@@ -0,0 +1,7 @@
+<div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum dolorum aperiam cum, nemo saepe ipsam libero quasi laboriosam fugiat, suscipit delectus dolorem perspiciatis doloribus, quidem blanditiis repellat nobis nam cupiditate!
+    Lorem, ipsum dolor sit amet consectetur adipisicing elit. Iste consequuntur tempora eum repellat nesciunt, totam, odit repellendup, cum non alias atque suscipit nam asperiores sapiente! Voluptatem dolores consectetur qui consequuntur.
+</div>
+,<div>
+   Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat, odio corporis! Numquam, dicta nisi? Voluptate repellendus nesciunt, esse hic sequi obcaecati dolore soluta totam? Aliquam minima officiis velit rem molestias! 
+</div>
