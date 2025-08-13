5604d30a04eededf4f50ec5a90feef9e85c45dbb
Dineesha-1 <ishtamithra@gmail.com>
2025-08-13 11:28:34 +0530

    Booking.jsx created

---
+++ Pick-and-Go/frontend/src/pages/Booking/Booking.jsx
@@ -0,0 +1,3 @@
+<div>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit eos labore tempore reiciendis excepturi nam? Illum, cum, repellendus perspiciatis modi iusto dolore unde, eligendi dolor similique dicta ipsa aspernatur assumenda?
+</div>
